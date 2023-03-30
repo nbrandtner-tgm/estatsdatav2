04d1f9457b4fc1e760fbfd44fea2ca23952a7481
nbrandtner-tgm <nbrandtner@student.tgm.ac.at>
2023-03-30 22:44:08 +0200

    zurücksetzen auf MySQLnew als Main Klasse aufgrund von Fehlern

--- src/main/java/org/example/MySQLnew.java
+++ src/main/java/org/example/MySQLnew.java
@@ -6,6 +6,7 @@ import java.time.*;
 import java.util.Date;
 
 public class MySQLnew {
+    // Timestamp erstellen
     private static final SimpleDateFormat sdf = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss");
     /**
      * @param args the command line arguments
@@ -15,17 +16,17 @@ public class MySQLnew {
         Date date;
         Timestamp timestamp;
         Timestamp timestampDelete;
-        // Connection zu MySQL aufbauen
+        // Connection zu MySQL aufbauen:
         Connection conn = null;
         Statement stmt = null;
         try {
             try {
-                // Treiber damit JDBC funktioniert
+                // Treiber damit JDBC funktioniert:
                 Class.forName("com.mysql.cj.jdbc.Driver");
             } catch (Exception e) {
                 System.out.println(e);
             }
-            // Verbindung zur Datenbank aufbauen
+            // Verbindung zur Datenbank aufbauen:
 
             //USE FIRST LINE IF YOU RUN THE PROGRAM ON YOUR LOCAL MACHINE (Remove // in front of the line)
             //conn = (Connection) DriverManager.getConnection("jdbc:mysql://localhost:3306/estatsdb", "root", "root");
@@ -35,7 +36,9 @@ public class MySQLnew {
             System.out.println("Connection is created successfully:");
             boolean alreadydone = false;
             while (true) {
+                // Aktuellen timestamp erstellen
                 LocalTime time = LocalTime.now();
+                // Wartet bis zur nächsten Stunde
                 if (time.getMinute() < 59) {
                     Thread.sleep(60000);
                 } else {
@@ -43,18 +46,20 @@ public class MySQLnew {
                         Thread.sleep(10000);
                     }
                 }
-                // (add this line when not testing anymore, will change save intervall from minutely to hourly)
+                // Wenn eine neue Stunde beginnt, werden die Daten generiert und in die Datenbank eingefuegt
                 if (time.getMinute() == 0 && time.getSecond() == 0 && alreadydone == false) {
                     System.out.println("It's a new hour!");
                     // Timestamp erstellen
                     date = new Date();
                     timestamp = new Timestamp(date.getTime());
-                    // Timestamp von gestern erstellen um die Daten, die aelter als gestern sind zu loeschen
+                    // Timestamp von gestern erstellen um die Daten, welche die aelter als ein Monat sind zu loeschen
                     Instant instant = timestamp.toInstant().minus(java.time.Duration.ofDays(30));
                     timestampDelete = Timestamp.from(instant);
                     System.out.println(sdf.format(timestamp) + " oder " + timestamp);
                     //Statement machen und Generator aufrufen
                     stmt = conn.createStatement();
+
+                    // Setzen der Variablen fuer die Gesamtstatistiken
                     String state = "";
                     double stromverbrauch = 0;
                     int strompreis = 0;
@@ -98,7 +103,7 @@ public class MySQLnew {
                                 state = "Wien";
                                 break;
                         }
-                        // Query fuer jedes Bundesland erstellen und ausfuehren
+                        // Query, welche die Daten in die Datenbank einfuegt, fuer jedes Bundesland erstellen und ausfuehren
                         String genquery = "INSERT INTO estats " + "VALUES ('" + state + "',0.0," + data[0] + "," + preis + "," + data[2] + "," + data[3] + "," + data[4] + ",'" + sdf.format(timestamp) + "');";
                         stmt.executeUpdate(genquery);
                         System.out.println("Query für: " + state + " erfolgreich");
@@ -109,25 +114,27 @@ public class MySQLnew {
                         stromexport += data[4];
                     }
 
-
+                    // Gesamtstatistiken fuer ganz Österreich machen:
                     // Strompreis Durchschnitt berechnen
                     strompreis = strompreis / 9;
                     // Query fuer ganz Österreich erstellen und ausfuehren
                     String queryAll = "INSERT INTO estats " + "VALUES ('" + "Oesterreich" + "',100.0," + stromverbrauch + "," + strompreis + "," + co2Emissionen + "," + stromimport + "," + stromexport + ",'" + sdf.format(timestamp) + "')";
                     System.out.println(queryAll);
                     stmt.executeUpdate(queryAll);
-                    // Gesamtstatistiken fuer ganz Österreich machen
                     System.out.println("Record is inserted in the table successfully..................");
                     System.out.println("Please check it in the MySQL Table..........");
 
-                    // Stromverbrauch Anteile berechnen
+                    // Stromverbrauch Anteile berechnen:
                     double oesterreichVerbrauch = 0;
+                    // Stromverbrauch von ganz Österreich auslesen
                     ResultSet oesterreichResSet = stmt.executeQuery("SELECT * FROM estats WHERE date='" + sdf.format(timestamp) + "' AND region='Oesterreich'");
                     while (oesterreichResSet.next()) {
                         oesterreichVerbrauch = oesterreichResSet.getDouble("verbrauch");
                     }
                     Statement anteilStatement = conn.createStatement();
+                    // Alle Bundeslaender auslesen und den Stromverbrauch Anteil berechnen
                     ResultSet anteilquery = anteilStatement.executeQuery("SELECT * FROM estats WHERE date='" + sdf.format(timestamp) + "' AND region!='Oesterreich'");
+                    // Stromverbrauch Anteil in die Datenbank schreiben
                     while (anteilquery.next()) {
                         String region = anteilquery.getString("region");
                         double verbrauch = anteilquery.getDouble("verbrauch");
@@ -135,7 +142,7 @@ public class MySQLnew {
                         stmt.executeUpdate(sqlAnteil);
                     }
                     System.out.println(anteilStatement);
-                    // Daten loeschen die aelter als gestern sind
+                    // Daten loeschen die aelter als 30 Tage sind
                     String sql = "DELETE FROM estats WHERE date < '" + sdf.format(timestampDelete) + "'";
                     int anzahlGeloeschterDatensaetze = stmt.executeUpdate(sql);
                     System.out.println(anzahlGeloeschterDatensaetze + " Datensätze wurden gelöscht.");
